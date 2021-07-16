7501f385b5c85919cb872696aaca72f832194800
Daniel Ogata <dankogata@gmail.com>
2021-07-16 14:41:11 -0300

    implementando TDD

---
+++ src/test/java/com/teste/AssertJTest.java
@@ -0,0 +1,16 @@
+package com.teste;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+/*
+public class AssertJTest {
+    @Test
+    public void checkEquality(){
+        Person person = new Person("Ogata","Daniel");
+        Person  personClose = person;
+
+        assertThat(person).isEqualTo(personClose);
+    }
+}
+ */
---
+++ src/test/java/com/teste/MatcherTest.java
@@ -0,0 +1,16 @@
+package com.teste;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+/*
+public class MatcherTest {
+    @Test
+    public void givenBean_checkToString(){
+        Person person = new Person("Ogata", "Daniel");
+        String str = person.toString();
+        Object HasToString;
+        assertThat(person, HasToString.hasToString(str));
+    }
+}
+ */
